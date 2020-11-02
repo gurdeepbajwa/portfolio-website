39f44fbcd750372b49092ba79e12cd93fc73e5f3
Gurdeep Bajwa <S3813563@student.rmit.edu.au>
2020-11-02 16:46:24 +1100

    Completed Form Validation
    Form will now validate inputs before submitting. Completed CSS for Form. Added Error messages.
    
    Website version 1.0

--- lambda_db_write.js
+++ lambda_db_write.js
@@ -19,33 +19,8 @@ function sendToAPI(obj) {
         .catch(error => console.log('error', error));
 }
 
-function clearForm() {
-    document.getElementById("messageInput").value = "";
-    document.getElementById("orgInput").value = "";
-    document.getElementById("SecondNameInput").value = "";
-    document.getElementById("firstNameInput").value = "";
-    document.getElementById("emailInput").value = "";
-}
-
-$('#submitButton').on('click', function () {
-    var email = document.getElementById("emailInput").value;
-    var firstName = document.getElementById("firstNameInput").value;
-    var secondName = document.getElementById("SecondNameInput").value;
-    var org = document.getElementById("orgInput").value;
-    var message = document.getElementById("messageInput").value;
-    var date = Date.now();
-
-    if (firstName == "" | firstName == null) {
-        firstName = "No name"
-    }
-    if (secondName == "" | secondName == null) {
-        secondName = "No name"
-    }
-    if (org == "" | org == null) {
-        org = "No org"
-    }
-
-    let JSONfile =
+function readyJSON(date, message, email, firstName, secondName, org) {
+    let obj =
     {
         "message": [
             {
@@ -59,8 +34,55 @@ $('#submitButton').on('click', function () {
         ]
     };
 
-    sendToAPI(JSONfile);
-    clearForm();
+    sendToAPI(obj);
+}
+
+function clearForm() {
+    document.getElementById("messageInput").value = "";
+    document.getElementById("orgInput").value = "";
+    document.getElementById("SecondNameInput").value = "";
+    document.getElementById("firstNameInput").value = "";
+    document.getElementById("emailInput").value = "";
+}
+
+$('#submitButton').on('click', function () {
+
+    // --- Validate Inputs -----------------------------------------------
+    var email = document.getElementById("emailInput").value;
+    var firstName = document.getElementById("firstNameInput").value;
+    var secondName = document.getElementById("SecondNameInput").value;
+    var org = document.getElementById("orgInput").value;
+    var message = document.getElementById("messageInput").value;
+    var date = Date.now();
+
+    let isInvalid = false;
+    var emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var emailRegExp = new RegExp(emailPattern);
+
+    // Reset error messages to empty
+    document.getElementById("emailErrorOutput").innerText = "";
+    document.getElementById("messageErrorOutput").innerText = "";
+
+    // Validation checks
+    if (email == null || email == "") {
+        isInvalid = true;
+        document.getElementById("emailErrorOutput").innerText = "Email address is mandatory";
+    } else if ( !emailRegExp.test(email)) {
+        isInvalid = true;
+        document.getElementById("emailErrorOutput").innerText = "Email address is not valid";
+    }
+    if (message == null || message == "") {
+        isInvalid = true;
+        document.getElementById("messageErrorOutput").innerText = "Message is mandatory";
+    }
+
+    // Valid  Inputs function
+    if (isInvalid == false) {
+        readyJSON(date, message, email, firstName, secondName, org);
+        clearForm();
+    }
+
+    // Prevent page reload
     return false;
 });
 
